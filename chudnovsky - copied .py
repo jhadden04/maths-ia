d4aabdf2392f051e0b4eb3c05fc4031384bb75b8
John Hadden <59323260+jhadden04@users.noreply.github.com>
2022-08-10 01:11:36 +0100

    Update chudnovsky - copied .py

--- chudnovsky - copied .py
+++ chudnovsky - copied .py
@@ -1,10 +1,22 @@
+# pretty much completely accurate, can avoid floating point problems etc
+"""
+Python3 program to calculate Pi using python long integers, and the
+Chudnovsky algorithm
+
+See: http://www.craig-wood.com/nick/articles/pi-chudnovsky/ for more
+info
+
+Nick Craig-Wood <nick@craig-wood.com>
+"""
+
 import math
-# https://www.craig-wood.com/nick/articles/pi-chudnovsky/
+from time import time
+
 
 def sqrt(n, one):
     """
     Return the square root of n as a fixed point number with the one
-    passed in.  It uses a second order Newton-Raphson convergence.  This
+    passed in.  It uses a second order Newton-Raphson convgence.  This
     doubles the number of significant figures on each iteration.
     """
     # Use floating point arithmetic to make an initial guess
@@ -18,55 +30,32 @@ def sqrt(n, one):
         if x == x_old:
             break
     return x
-def pi_chudnovsky_bs(digits):
-    """
-    Compute int(pi * 10**digits)
 
-    This is done using Chudnovsky's series with binary splitting
+
+def pi_chudnovsky(one=1000000):
     """
+    Calculate pi using Chudnovsky's series
+
+    This calculates it in fixed point, using the value for one passed in
+    """
+    k = 1
+    a_k = one
+    a_sum = one
+    b_sum = 0
     C = 640320
-    C3_OVER_24 = C**3 // 24
-    def bs(a, b):
-        """
-        Computes the terms for binary splitting the Chudnovsky infinite series
+    C3_OVER_24 = C ** 3 // 24
+    while 1:
+        a_k *= -(6 * k - 5) * (2 * k - 1) * (6 * k - 1)
+        a_k //= k * k * k * C3_OVER_24
+        a_sum += a_k
+        b_sum += k * a_k
+        k += 1
+        if a_k == 0:
+            break
+    total = 13591409 * a_sum + 545140134 * b_sum
+    pi = (426880 * sqrt(10005 * one, one) * one) // total
+    return pi
 
-        a(a) = +/- (13591409 + 545140134*a)
-        p(a) = (6*a-5)*(2*a-1)*(6*a-1)
-        b(a) = 1
-        q(a) = a*a*a*C3_OVER_24
 
-        returns P(a,b), Q(a,b) and T(a,b)
-        """
-        if b - a == 1:
-            # Directly compute P(a,a+1), Q(a,a+1) and T(a,a+1)
-            if a == 0:
-                Pab = Qab = 1
-            else:
-                Pab = (6*a-5)*(2*a-1)*(6*a-1)
-                Qab = a*a*a*C3_OVER_24
-            Tab = Pab * (13591409 + 545140134*a) # a(a) * p(a)
-            if a & 1:
-                Tab = -Tab
-        else:
-            # Recursively compute P(a,b), Q(a,b) and T(a,b)
-            # m is the midpoint of a and b
-            m = (a + b) // 2
-            # Recursively calculate P(a,m), Q(a,m) and T(a,m)
-            Pam, Qam, Tam = bs(a, m)
-            # Recursively calculate P(m,b), Q(m,b) and T(m,b)
-            Pmb, Qmb, Tmb = bs(m, b)
-            # Now combine
-            Pab = Pam * Pmb
-            Qab = Qam * Qmb
-            Tab = Qmb * Tam + Pam * Tmb
-        return Pab, Qab, Tab
-    # how many terms to compute
-    DIGITS_PER_TERM = math.log10(C3_OVER_24/6/2/6)
-    N = int(digits/DIGITS_PER_TERM + 1)
-    # Calclate P(0,N) and Q(0,N)
-    P, Q, T = bs(0, N)
-    one = 10**digits
-    sqrtC = sqrt(10005*one, one)
-    return (Q*426880*sqrtC) // T
+print(pi_chudnovsky(10 ** 100))
 
-print(pi_chudnovsky_bs(10000))
